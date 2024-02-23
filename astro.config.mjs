5eabca0fa9f385930dad82487b4106aa70ae399b
jhutchison-opm <jonathan.hutchison@opm.gov>
2024-02-23 14:41:25 -0500

    Update built assets dir so .nojekyll isnt needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ export default defineConfig({
   integrations: [react()],
   site: 'https://jhutchison-opm.github.io',
   base: '/ACRT-BART',
-  outDir: './docs' // For github pages manual deploy
+  outDir: './docs', // For github pages manual deploy
+  build: {
+    assets: 'assets'
+  }
 });
--- docs/assets/license.DhKNVr1t.js
+++ docs/assets/license.DhKNVr1t.js
@@ -1,7 +1,7 @@
 import{_ as t}from"./input-form.tbFjoukD.js";import"./jsx-runtime.G4i-1Giy.js";import"./index.UCAuQZPX.js";async function o(){return t(()=>import("./license.AHuLwwb3.js"),__vite__mapDeps([0,1,2,3,4]))}const e="@@ASTRO-LINKS@@",r="@@ASTRO-STYLES@@",c="@@ASTRO-SCRIPTS@@",n={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:r,collectedScripts:c};export{n as default};
 function __vite__mapDeps(indexes) {
   if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["_astro/license.AHuLwwb3.js","_astro/render-template.TrlrTYZL.js","_astro/input-form.tbFjoukD.js","_astro/jsx-runtime.G4i-1Giy.js","_astro/index.UCAuQZPX.js"]
+    __vite__mapDeps.viteFileDeps = ["assets/license.AHuLwwb3.js","assets/render-template.TrlrTYZL.js","assets/input-form.tbFjoukD.js","assets/jsx-runtime.G4i-1Giy.js","assets/index.UCAuQZPX.js"]
   }
   return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
 }
