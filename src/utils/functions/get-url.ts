3dd1cd91e88f86b69965d70516264cd086fdbfbe
jhutchison-opm <jonathan.hutchison@opm.gov>
2024-02-28 17:14:38 -0500

    Fix getUrl

--- src/utils/functions/get-url.ts
+++ src/utils/functions/get-url.ts
@@ -1,5 +1,8 @@
-export const getUrl = (path: string) => {
-  const basePath = new URL(import.meta.env.BASE_URL, "https://x")
-  const { pathname: relPath } = new URL(path, basePath)
-  return relPath
+type RelativePath = `/${string}`
+
+export const getUrl = (path: RelativePath) => {
+  const newPath = (import.meta.env.BASE_URL + path).replace(/\/+/g, '/')
+  const basePath = new URL(newPath, "https://x")
+
+  return basePath.pathname
 }
