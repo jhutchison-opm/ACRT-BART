f9791da7f8a7e5eaa04d568f90dc8179bd3823be
jhutchison-opm <jonathan.hutchison@opm.gov>
2024-04-11 12:21:02 -0400

    Update build

---
+++ docs/assets/home.UsM9HCG4.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./db.t17wDfli.js";import"./index.UCAuQZPX.js";async function o(){return t(()=>import("./home.eZLPRHjs.js"),__vite__mapDeps([0,1,2]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",n={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{n as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/home.eZLPRHjs.js","assets/db.t17wDfli.js","assets/index.UCAuQZPX.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ docs/assets/instructions.9eJnMC6s.js
@@ -0,0 +1,56 @@
+import{f as n,h as r,m as o,u as i}from"./db.t17wDfli.js";import"./index.UCAuQZPX.js";const t=`<h2 id="acrt-instructions">ACRT Instructions</h2>
+<p>To access ACRT User Guide:</p>
+<ol>
+<li>Locate the instructions in the Resources folder.(The instructions are available in Word and PDF formats.)</li>
+<li>Or Select ACRT User Guide to open within the application.</li>
+</ol>
+<h3 id="create-report">Create Report</h3>
+<ol>
+<li>Select Create Report in the menu to begin a new Report.</li>
+<li>Select Choose File and load a JSON file:
+<ul>
+<li>First time use Trusted Tester v.5.1.1 (for web) or Trusted Tester v.4 (for software) (TT5.1.1 or TT4) located in the ‘Resources’ folder.</li>
+<li>Or use previously created JSON report file.</li>
+</ul>
+</li>
+<li>Select Load File button.</li>
+<li>Open Product and Testing Information to complete each form and the required fields.</li>
+<li>Add/Edit Test Results.</li>
+<li>Select Save button or the keyboard shortcut (Alt+S).</li>
+</ol>
+<h3 id="view-report">View Report</h3>
+<ol>
+<li>Select View Report in the top menu to open an existing report that contains at least one valid test result.</li>
+<li>Select Choose File and load JSON file with test results.</li>
+<li>Select Open.</li>
+<li>Select Load Results button.</li>
+<li>View the Test Results.</li>
+<li>Select the Save as HTML button at the bottom or use Alt+S to generate a printer-friendly version of the report and save JSON file for future use.</li>
+</ol>`,s={title:"Instructions"},l="C:/Users/JRHUTCHISON/OneDrive - US Office of Personnel Management/Documents/GitHub/ACRT-BART/src/content/pages/instructions.md",a="/ACRT-BART/instructions";function T(){return`\r
+## ACRT Instructions\r
+\r
+To access ACRT User Guide:\r
+\r
+1. Locate the instructions in the Resources folder.(The instructions are available in Word and PDF formats.)\r
+2. Or Select ACRT User Guide to open within the application.\r
+\r
+### Create Report\r
+\r
+1. Select Create Report in the menu to begin a new Report.\r
+2. Select Choose File and load a JSON file:\r
+    - First time use Trusted Tester v.5.1.1 (for web) or Trusted Tester v.4 (for software) (TT5.1.1 or TT4) located in the 'Resources' folder.\r
+    - Or use previously created JSON report file.\r
+3. Select Load File button.\r
+4. Open Product and Testing Information to complete each form and the required fields.\r
+5. Add/Edit Test Results.\r
+6. Select Save button or the keyboard shortcut (Alt+S).\r
+\r
+### View Report\r
+\r
+1. Select View Report in the top menu to open an existing report that contains at least one valid test result.\r
+2. Select Choose File and load JSON file with test results.\r
+3. Select Open.\r
+4. Select Load Results button.\r
+5. View the Test Results.\r
+6. Select the Save as HTML button at the bottom or use Alt+S to generate a printer-friendly version of the report and save JSON file for future use.\r
+`}function S(){return t}function R(){return[{depth:2,slug:"acrt-instructions",text:"ACRT Instructions"},{depth:3,slug:"create-report",text:"Create Report"},{depth:3,slug:"view-report",text:"View Report"}]}const m=n((u,c,d)=>{const{layout:p,...e}=s;return e.file=l,e.url=a,r`${o()}${i(t)}`});export{m as Content,S as compiledContent,m as default,l as file,s as frontmatter,R as getHeadings,T as rawContent,a as url};
